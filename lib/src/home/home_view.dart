d0d7a56f12be5e210ca3288eefee687d7e0e154b
yami-beta <yami-beta@users.noreply.github.com>
2022-01-05 10:29:24 +0900

    chore: 不要なコードの削除

---
+++ lib/src/home/home_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../settings/settings_view.dart';
+
+/// Displays a list of SampleItems.
+class HomeView extends StatelessWidget {
+  const HomeView({
+    Key? key,
+  }) : super(key: key);
+
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // Navigate to the settings page. If the user leaves and returns
+              // to the app after it has been killed while running in the
+              // background, the navigation stack is restored.
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
